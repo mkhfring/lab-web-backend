5801716db6c5ef29ea3e480514d4a224563a3a47
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-07-23 00:24:13 -0700

    Removing unnecessary files

--- flasker/config.py
+++ flasker/config.py
@@ -26,7 +26,6 @@ class DeploymentConfig(Config):
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
-    import pudb; pudb.set_trace()  # XXX BREAKPOINT
     TESTING = True
     DEBUG = True
 
